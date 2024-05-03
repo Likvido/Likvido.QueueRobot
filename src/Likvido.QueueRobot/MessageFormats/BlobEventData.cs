f8ee5982760ee2712287d736fc2cbf80e40ddfe5
Søren Pedersen <sh.pedersen@gmail.com>
2024-05-03 16:46:39 +0200

    Add the message format BlobEventData, as this is a common format, which is useful to have in many robots

---
+++ src/Likvido.QueueRobot/MessageFormats/BlobEventData.cs
@@ -0,0 +1,20 @@
+namespace Likvido.QueueRobot.MessageFormats;
+
+public class BlobEventData
+{
+    public string Api { get; set; } = null!;
+    public string ClientRequestId { get; set; } = null!;
+    public string RequestId { get; set; } = null!;
+    public string ETag { get; set; } = null!;
+    public string ContentType { get; set; } = null!;
+    public int ContentLength { get; set; }
+    public string BlobType { get; set; } = null!;
+    public string Url { get; set; } = null!;
+    public string Sequencer { get; set; } = null!;
+    public StorageDiagnostics StorageDiagnostics { get; set; } = null!;
+}
+
+public class StorageDiagnostics
+{
+    public string BatchId { get; set; } = null!;
+}
